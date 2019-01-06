ea84d9b6bc3c29b2c698024f8cacdbfc750fa4c5
Raymond Gregory <vinedsel@gmail.com>
2019-01-06 16:12:18 -0600

    Added navigation code to index file.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-alert("it's alive");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './screens/Home';
+import About from './screens/About';
+import FavoriteThings from './screens/FavoriteThings';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const App = () => (
+  <Router>
+    <div>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/things">Things</Link>
+      </li>
+    </ul>
+
+    <hr />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/topics" component={Topics} />
+    </div>
+  </Router>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
